990c648b5495d31e0d47087b1ce8b0a661d2ef45
Michael Meyer <michael@mtmeyer.net>
2023-12-30 15:13:09 +1100

    feat: add snake collisions and snack collection

--- src/snake.zig
+++ src/snake.zig
@@ -6,17 +6,19 @@ const tileSize = 16;
 const screenWidth = tileSize * 80;
 const screenHeight = tileSize * 45;
 
-const MovementDirection = enum { Up, Down, Left, Right };
+const MovementDirection = enum { Up, Down, Left, Right, None };
 
 fn setupWindow() void {
     raylib.SetConfigFlags(raylib.ConfigFlags{ .FLAG_WINDOW_RESIZABLE = false });
     raylib.InitWindow(screenWidth, screenHeight, "Zig Snek");
-    raylib.SetTargetFPS(10);
+    raylib.SetTargetFPS(60);
 }
 
 pub fn playGame() !void {
     var snakeSegments = std.ArrayList(raylib.Vector2i).init(std.heap.c_allocator);
     defer snakeSegments.deinit();
+
+    var snack: raylib.Vector2i = .{ .x = 960, .y = 480 };
 
     try snakeSegments.append(.{ .x = 96, .y = 96 });
     try snakeSegments.append(.{ .x = 80, .y = 96 });
@@ -24,47 +26,97 @@ pub fn playGame() !void {
     try snakeSegments.append(.{ .x = 64, .y = 80 });
     try snakeSegments.append(.{ .x = 64, .y = 64 });
     try snakeSegments.append(.{ .x = 64, .y = 48 });
+    try snakeSegments.append(.{ .x = 80, .y = 48 });
+    try snakeSegments.append(.{ .x = 96, .y = 48 });
 
     setupWindow();
     defer raylib.CloseWindow();
 
     var currentMoveDirection = MovementDirection.Right;
+    var playerInputDirection = MovementDirection.None;
+
+    // Arbitrary high number to satisfy if loop on game launch
+    var timeSinceLastTick: f32 = 100.0;
 
     while (!raylib.WindowShouldClose()) {
-        var dt = raylib.GetFrameTime();
-        _ = dt;
-
-        currentMoveDirection = try moveSnake(&snakeSegments, currentMoveDirection);
-
         raylib.BeginDrawing();
         defer raylib.EndDrawing();
 
+        var dt = raylib.GetFrameTime();
+
+        const tickRate: f32 = dt * 5;
+        raylib.DrawFPS(10, 10);
+
         raylib.ClearBackground(raylib.BLACK);
 
-        renderSnake(snakeSegments);
+        playerInputDirection = getPlayerInput(playerInputDirection);
+
+        if (timeSinceLastTick >= tickRate) {
+            currentMoveDirection = try moveSnake(&snakeSegments, currentMoveDirection, playerInputDirection);
+
+            timeSinceLastTick = 0;
+        } else {
+            timeSinceLastTick += dt;
+        }
+
+        handleCollisionsWithSelf(&snakeSegments);
+        snack = try handleCollisionWithSnack(&snakeSegments, snack);
+
+        renderSnake(&snakeSegments);
+
+        raylib.DrawRectangle(snack.x, snack.y, tileSize, tileSize, raylib.LIME);
     }
 }
 
-fn moveSnake(snakeSegments: *std.ArrayList(raylib.Vector2i), currentMoveDirection: MovementDirection) !MovementDirection {
+fn getPlayerInput(playerInputDirection: MovementDirection) MovementDirection {
+    if (raylib.IsKeyDown(.KEY_W)) {
+        return MovementDirection.Up;
+    } else if (raylib.IsKeyDown(.KEY_S)) {
+        return MovementDirection.Down;
+    } else if (raylib.IsKeyDown(.KEY_D)) {
+        return MovementDirection.Right;
+    } else if (raylib.IsKeyDown(.KEY_A)) {
+        return MovementDirection.Left;
+    } else {
+        return playerInputDirection;
+    }
+}
+
+fn moveSnake(snakeSegments: *std.ArrayList(raylib.Vector2i), currentMoveDirection: MovementDirection, playerInputDirection: MovementDirection) !MovementDirection {
     var moveChangeVector: raylib.Vector2i = .{ .x = 0, .y = 0 };
     var finalMoveDir: MovementDirection = MovementDirection.Right;
 
-    if (raylib.IsKeyDown(.KEY_W) and currentMoveDirection != MovementDirection.Down) {
-        moveChangeVector.y -= 1 * tileSize;
-        finalMoveDir = MovementDirection.Up;
-    } else if (raylib.IsKeyDown(.KEY_S) and currentMoveDirection != MovementDirection.Up) {
-        moveChangeVector.y += 1 * tileSize;
-        finalMoveDir = MovementDirection.Down;
-    } else if (raylib.IsKeyDown(.KEY_D) and currentMoveDirection != MovementDirection.Left) {
-        moveChangeVector.x += 1 * tileSize;
-        finalMoveDir = MovementDirection.Right;
-    } else if (raylib.IsKeyDown(.KEY_A) and currentMoveDirection != MovementDirection.Right) {
-        moveChangeVector.x -= 1 * tileSize;
-        finalMoveDir = MovementDirection.Left;
+    switch (playerInputDirection) {
+        .Up => {
+            if (currentMoveDirection != MovementDirection.Down) {
+                moveChangeVector.y -= 1 * tileSize;
+                finalMoveDir = MovementDirection.Up;
+            }
+        },
+        .Down => {
+            if (currentMoveDirection != MovementDirection.Up) {
+                moveChangeVector.y += 1 * tileSize;
+                finalMoveDir = MovementDirection.Down;
+            }
+        },
+        .Left => {
+            if (currentMoveDirection != MovementDirection.Right) {
+                moveChangeVector.x -= 1 * tileSize;
+                finalMoveDir = MovementDirection.Left;
+            }
+        },
+        .Right => {
+            if (currentMoveDirection != MovementDirection.Left) {
+                moveChangeVector.x += 1 * tileSize;
+                finalMoveDir = MovementDirection.Right;
+            }
+        },
+        .None => {
+            finalMoveDir = currentMoveDirection;
+        },
     }
 
     if (moveChangeVector.x == 0 and moveChangeVector.y == 0) {
-        finalMoveDir = currentMoveDirection;
         switch (currentMoveDirection) {
             .Up => {
                 moveChangeVector.y -= 1 * tileSize;
@@ -78,6 +130,7 @@ fn moveSnake(snakeSegments: *std.ArrayList(raylib.Vector2i), currentMoveDirectio
             .Right => {
                 moveChangeVector.x += 1 * tileSize;
             },
+            .None => {},
         }
     }
 
@@ -94,7 +147,7 @@ fn moveSnake(snakeSegments: *std.ArrayList(raylib.Vector2i), currentMoveDirectio
     return finalMoveDir;
 }
 
-fn renderSnake(snakeSegments: std.ArrayList(raylib.Vector2i)) void {
+fn renderSnake(snakeSegments: *std.ArrayList(raylib.Vector2i)) void {
     for (0..snakeSegments.items.len, snakeSegments.items) |i, segment| {
         if (i == 0) {
             raylib.DrawRectangle(segment.x, segment.y, tileSize, tileSize, raylib.SKYBLUE);
@@ -102,4 +155,50 @@ fn renderSnake(snakeSegments: std.ArrayList(raylib.Vector2i)) void {
             raylib.DrawRectangle(segment.x, segment.y, tileSize, tileSize, raylib.WHITE);
         }
     }
+}
+
+fn handleCollisionsWithSelf(snakeSegments: *std.ArrayList(raylib.Vector2i)) void {
+    // Snake can only collide with itself after 4 segments exist
+    if (snakeSegments.items.len < 5) {
+        return;
+    }
+
+    const snakeHead: raylib.Rectangle = rect(snakeSegments.items[0]);
+
+    for (0..snakeSegments.items.len, snakeSegments.items) |i, segment| {
+        if (i < 4) {
+            continue;
+        }
+        const hasCollided = raylib.CheckCollisionRecs(snakeHead, rect(segment));
+        if (hasCollided) {
+            gameOver();
+        }
+    }
+}
+
+fn handleCollisionWithSnack(snakeSegments: *std.ArrayList(raylib.Vector2i), snack: raylib.Vector2i) !raylib.Vector2i {
+    const head = snakeSegments.items[0];
+    if (raylib.CheckCollisionRecs(rect(head), rect(snack))) {
+        const lastSegment = snakeSegments.items[snakeSegments.items.len - 1];
+        const secondLastSegment = snakeSegments.items[snakeSegments.items.len - 2];
+        const diff: raylib.Vector2i = .{ .x = lastSegment.x - secondLastSegment.x, .y = lastSegment.y - secondLastSegment.y };
+
+        const newSegment: raylib.Vector2i = .{ .x = lastSegment.x + diff.x, .y = lastSegment.y + diff.y };
+
+        try snakeSegments.append(newSegment);
+
+        return .{ .x = -100, .y = -100 };
+    } else {
+        return snack;
+    }
+}
+
+fn gameOver() void {
+    std.debug.print("Game Over", .{});
+    // raylib.CloseWindow();
+}
+
+fn rect(vector: raylib.Vector2i) raylib.Rectangle {
+    const returnValue: raylib.Rectangle = .{ .x = @floatFromInt(vector.x), .y = @floatFromInt(vector.y), .width = tileSize, .height = tileSize };
+    return returnValue;
 }
